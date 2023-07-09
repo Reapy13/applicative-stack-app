eab415d28fd864fe240db3fbe745e199891aca7c
hfauchery <hugo.fauchery2@soprasteria.com>
2023-07-09 08:11:36 +0200

    feat(init-angular-stack): Changes the name of ast-ng to asa-ng
    feat(init-angular-stack): Changes the name of ast-ng to asa-ng

--- asa-ng/karma.conf.js
+++ asa-ng/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ast-ng'),
+      dir: require('path').join(__dirname, './coverage/asa-ng'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- asa-ng/src/app/app.component.spec.ts
+++ asa-ng/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ast-ng'`, () => {
+  it(`should have as title 'asa-ng'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ast-ng');
+    expect(app.title).toEqual('asa-ng');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ast-ng app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('asa-ng app is running!');
   });
 });
